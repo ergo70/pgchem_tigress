f063ad583cdb87146e5aeb870e5b17352de7b48f
Ernst-Georg Schmid <pgchem@tuschehund.de>
2018-02-21 14:34:57 +0100

    Add support for PostgreSQL 10.x

--- src/reaction/reaction_io.c
+++ src/reaction/reaction_io.c
@@ -16,8 +16,8 @@
  ************************************************************************/
 #include <time.h>
 #include "postgres.h"
-#include "libpq/md5.h" // PostgreSQL 9.x
-//#include "common/md5.h" //PostgreSQL 10.x
+//#include "libpq/md5.h" // PostgreSQL 9.x
+#include "common/md5.h" //PostgreSQL 10.x
 #include "fmgr.h"
 #include "libpq/pqformat.h"	/* needed for send/recv functions */
 #include "reaction.h"
